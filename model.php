70824a1756abf17e485adfa90e37d41f764145d8
Nicoandriatina <andriatina24@gmail,com>
2022-09-23 18:18:22 +0300

    premier modification

---
+++ model.php
@@ -0,0 +1,31 @@
+<?php
+
+class Database 
+{
+    private $host = 'mysql:dbname:crud_ajax';
+    private $user = 'root';
+    private $password ='root';
+
+    private function getconnexion()
+{
+    try {
+        return new PDO($this->host, $this->user, $this->password);
+    } catch (PDOException $e) {
+        die('erreur:'.$e->getMessage());
+    }
+}
+    public function create(string $customer, string $cashier, int $amount, int $received, int $returned, string $state)
+    {
+        $q = $this->getconnexion()->prepare("INSERT INTO factures(customer, cashier, amount, received,returned,state)
+         VALUES (:customer, :cashier, :amount, :received, :returned, :state)");
+        return $q->execute([
+            'customer' => $customer,
+            'cashier' => $cashier,
+            'amount' => $amount,
+            'received' => $received,
+            'returned' => $returned,
+            'state' => $state
+        ]);
+    }
+}
+
