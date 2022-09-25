f864d4e40a27be1cdb4cd0040b64f14e5493f3f3
M3ndrika74 <ainarakotomalala227@gmail.com>
2022-09-25 19:53:31 +0100

    [create] debug pour la creation du facture

---
+++ crud_ajax.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : dim. 25 sep. 2022 à 19:01
+-- Version du serveur : 10.4.22-MariaDB
+-- Version de PHP : 7.3.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `crud_ajax`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `facture`
+--
+
+CREATE TABLE `facture` (
+  `id` int(20) NOT NULL,
+  `customer` varchar(100) NOT NULL,
+  `cashier` varchar(200) NOT NULL,
+  `amount` int(10) NOT NULL,
+  `reseived` int(10) NOT NULL,
+  `returned` int(10) NOT NULL,
+  `state` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `facture`
+--
+ALTER TABLE `facture`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `facture`
+--
+ALTER TABLE `facture`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- model.php
+++ model.php
@@ -1,22 +1,23 @@
 <?php
 
-class Database 
+class Database
 {
-    private $host = 'mysql:dbname:crud_ajax';
+    // erreur tato le host, nom table facture sans "s" ary reseived
+    private $host = 'mysql:host=localhost;dbname=crud_ajax';
     private $user = 'root';
-    private $password ='root';
+    private $password = '';
 
     private function getconnexion()
-{
-    try {
-        return new PDO($this->host, $this->user, $this->password);
-    } catch (PDOException $e) {
-        die('erreur:'.$e->getMessage());
+    {
+        try {
+            return new PDO($this->host, $this->user, $this->password);
+        } catch (PDOException $e) {
+            die('erreur:' . $e->getMessage());
+        }
     }
-}
     public function create(string $customer, string $cashier, int $amount, int $received, int $returned, string $state)
     {
-        $q = $this->getconnexion()->prepare("INSERT INTO factures(customer, cashier, amount, received,returned,state)
+        $q = $this->getconnexion()->prepare("INSERT INTO facture(customer, cashier, amount, reseived,returned,state)
          VALUES (:customer, :cashier, :amount, :received, :returned, :state)");
         return $q->execute([
             'customer' => $customer,
@@ -28,4 +29,3 @@ class Database
         ]);
     }
 }
-
