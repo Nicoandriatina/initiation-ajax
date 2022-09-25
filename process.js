96e1ecdd911e5067eda73678a7fe5670a57118a3
Nicoandriatina <andriatina24@gmail,com>
2022-09-25 17:09:50 +0300

    modif2

---
+++ process.js
@@ -0,0 +1,18 @@
+$(function() {
+    $('table').DataTable();
+
+    //creation d'un facture
+    $('#create').on('click', function(e){
+      let formOrder = $('#formOrder')
+      if(formOrder [0].checkValidity())
+      e.preventDefault();
+      $.ajax({
+         url: 'process.php',
+         type: 'post',  
+         data: formOrder.serialize() + '&action=create',
+         success: function(response){
+            console.log(response);
+         }
+      }) 
+    }) 
+ })
